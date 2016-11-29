cbd6ac86016c3ed4cedc50a83925bcf70761b459
Jesusbaby <olayemifolakemi@yahoo.com>
2016-11-29 15:57:50 +0100

    all tests passed

--- wordCount/wordCount.js
+++ wordCount/wordCount.js
@@ -3,7 +3,11 @@ var app = {
     var words = value.replace(/[.]+/g, '').split(/\s+/);
     result = {};
     for (var i = 0; i < words.length; i++) {
-      result[words[i]] = (result[words[i]] || 0) + 1;
+      if (typeof(result[words[i]]) != 'function') {
+        result[words[i]] = (result[words[i]] || 0) + 1;
+      } else {
+        result[words[i]] = (result['"' + words[i] + '"'] || 0) + 1;
+      }
     }
     return result;
   }
